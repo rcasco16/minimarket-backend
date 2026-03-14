aebd73d6df3ea5e62c277f3f7e36d99242dbc97a
Rodrigo Casco <rodrigocasco666@gmail.com>
2026-03-14 15:13:01 -0300

    Corregir importación de registrarEmpresa y asegurar bcrypt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+// 👇 IMPORTAMOS LAS DOS FUNCIONES AQUÍ 👇
+const { login, registrarEmpresa } = require('../controllers/authController');
 
 // Ruta para iniciar sesión
 router.post('/login', login);
 
+// Ruta para registrar nuevos clientes (empresas)
 router.post('/registrar-cliente', registrarEmpresa);
 
 module.exports = router;
